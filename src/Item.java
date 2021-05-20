5b7e3074e09e92057e4f4b93cff2110b32f8f8ca
Tira Sundara <sundaralinus@gmail.com>
2021-05-21 06:50:19 +0700

    Initial commit

---
+++ src/Item.java
@@ -0,0 +1,17 @@
+public class Item {
+    private String name;
+    private Integer weight;
+
+    Item(String name, Integer weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public Integer getWeight() {
+        return this.weight;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
