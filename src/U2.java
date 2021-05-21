1166c99c3914009fdf77859ccc2f999db51a18b9
Tira Sundara <sundaralinus@gmail.com>
2021-05-21 08:49:11 +0700

    Add U2 rocket class

---
+++ src/U2.java
@@ -0,0 +1,26 @@
+public class U2 extends Rocket {
+
+    U2() {
+        this.cost = 120;
+        this.weight = 18000;
+        this.maxWeight = 29000;
+        this.launchExplosionPercentage = 0.04;
+        this.landingCrashPercentage = 0.08;
+    }
+
+    @Override
+    public boolean launch() {
+        Integer random = (int) Math.random() * 100 + 1;
+        Double explosionChance = this.launchExplosionPercentage * (this.cargoCarried() / this.cargoLimit());
+
+        return explosionChance <= random;
+    }
+
+    @Override
+    public boolean land() {
+        Integer random = (int) Math.random() * 100 + 1;
+        Double explosionChance = this.landingCrashPercentage * (this.cargoCarried() / this.cargoLimit());
+
+        return explosionChance <= random;
+    }
+}
