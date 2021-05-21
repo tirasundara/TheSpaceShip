656ea184d124df572f377fac562138b2ad2ccc47
Tira Sundara <sundaralinus@gmail.com>
2021-05-21 08:48:55 +0700

    Add U1 rocket class

--- src/U1.java
+++ src/U1.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class U1 {
+public class U1 extends Rocket {
+
+    U1() {
+        this.cost = 100;
+        this.weight = 10000;
+        this.maxWeight = 18000;
+        this.launchExplosionPercentage = 0.05;
+        this.landingCrashPercentage = 0.01;
+    }
+
+    @Override
+    public boolean launch() {
+        Integer random = (int) Math.random() * 100 + 1;
+        Double explosionChance = this.launchExplosionPercentage * (this.cargoCarried() / this.cargoLimit());
+
+        return explosionChance <= random;
+    }
+
+    @Override
+    public boolean land() {
+        Integer random = (int) Math.random() * 100 + 1;
+        Double explosionChance = this.landingCrashPercentage * (this.cargoCarried() / this.cargoLimit());
+
+        return explosionChance <= random;
+    }
 }
